ccafd34a284ffa1f8420ad2132fddb8a4712261f
Bryce Cater <brycedcarter@gmail.com>
2016-04-25 18:02:42 -0400

    adding shared header with command types

---
+++ EmbeddedAudioSharedTypes.h
@@ -0,0 +1,29 @@
+/*
+ * EmbeddedAudioSharedTypes.h
+ *
+ *  Created on: Apr 25, 2016
+ *      Author: Bryce Carter
+ */
+
+#ifndef EMBEDDEDAUDIOSHAREDTYPES_H_
+#define EMBEDDEDAUDIOSHAREDTYPES_H_
+
+typedef enum command_type
+{
+	MUTE,
+	UNMUTE,
+	PLAY,
+	PAUSE,
+	FLUSH_BUFFER,
+	SET_VOLUME
+
+} command_type_t;
+
+typedef struct control_command
+{
+	command_type_t type;
+	int value;
+} control_command_t;
+
+
+#endif /* EMBEDDEDAUDIOSHAREDTYPES_H_ */
