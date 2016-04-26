0a4f6e3c28b3f32d5796e55ff1202f24d4db2d56
Bryce Cater <brycedcarter@gmail.com>
2016-04-25 20:03:04 -0400

    idk

--- EmbeddedAudioSharedTypes.h
+++ EmbeddedAudioSharedTypes.h
@@ -5,25 +5,25 @@
  *      Author: Bryce Carter
  */
 
-#ifndef EMBEDDEDAUDIOSHAREDTYPES_H_
-#define EMBEDDEDAUDIOSHAREDTYPES_H_
-
-typedef enum command_type
-{
-	MUTE,
-	UNMUTE,
-	PLAY,
-	PAUSE,
-	FLUSH_BUFFER,
-	SET_VOLUME
-
-} command_type_t;
-
-typedef struct control_command
-{
-	command_type_t type;
-	int value;
-} control_command_t;
-
-
-#endif /* EMBEDDEDAUDIOSHAREDTYPES_H_ */
+//#ifndef EMBEDDEDAUDIOSHAREDTYPES_H_
+//#define EMBEDDEDAUDIOSHAREDTYPES_H_
+//
+//typedef enum command_type
+//{
+//	MUTE,
+//	UNMUTE,
+//	PLAY,
+//	PAUSE,
+//	FLUSH_BUFFER,
+//	SET_VOLUME
+//
+//} command_type_t;
+//
+//typedef struct control_command
+//{
+//	command_type_t type;
+//	int value;
+//} control_command_t;
+//
+//
+//#endif /* EMBEDDEDAUDIOSHAREDTYPES_H_ */
